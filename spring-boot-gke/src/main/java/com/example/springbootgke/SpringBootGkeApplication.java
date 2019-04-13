d465c19226a46b8f57a3f10e197f6331349c9203
Karthik <karthik@email.com>
2019-04-13 12:26:37 -0400

    initial commit

---
+++ spring-boot-gke/src/main/java/com/example/springbootgke/SpringBootGkeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGkeApplication.class, args);
+	}
+
+}
