bb6f34db87d418e244ae5f5e63d15747396504f5
Karthik <karthik@email.com>
2019-04-27 17:33:53 -0400

    Modified to read environment variable on k8s

--- spring-app/src/main/java/com/example/springapp/HelloController.java
+++ spring-app/src/main/java/com/example/springapp/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.springapp;
 
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloController implements EnvironmentAware {
+	
+	private Environment environment;
 
 	@GetMapping(path = "/")
-	public String hello() {
-		return "Hello World GKE!!";
+	public String hello() {		
+		return environment.getProperty("Service","Hello World GKE!!");
 	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
+	}
+	
+	
 
 }
